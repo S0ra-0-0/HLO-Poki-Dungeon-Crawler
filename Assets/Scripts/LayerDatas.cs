b5740285582db653bd11c85a661544b8fb988179
OrakGarak2 <888seonghun888@gmail.com>
2025-09-16 09:58:33 +0200

    Add | Room Chage System

--- Assets/Scripts/LayerDatas.cs
+++ Assets/Scripts/LayerDatas.cs
@@ -3,5 +3,7 @@ namespace HLO.Layer
     public static class LayerDatas
     {
         public const int PLAYER_LAYER = 3;
+        public const int DOOR_LAYER = 7;
+        public const int ROOM_LAYER = 8;
     }
 }
---
+++ Assets/Scripts/Room/TempDungeonSetter.cs
@@ -0,0 +1,61 @@
+// System
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// Unity
+using UnityEngine;
+
+// HLO
+using HLO.Room;
+using HLO.Door;
+using HLO.Layer;
+
+public class TempDungeonSetter : MonoBehaviour
+{
+    [SerializeField] private Transform dungeon;
+
+    private void Start()
+    {
+        foreach (var room in dungeon.GetComponentsInChildren<RoomBase>())
+        {
+            foreach (var door in room.DoorList)
+            {
+                float angle = 0f;
+                switch (door.DoorDirectionType)
+                {
+                    case DoorDirectionType.Left:
+                        angle = 90f;
+                        break;
+                    case DoorDirectionType.Right:
+                        angle = 270f;
+                        break;
+                    case DoorDirectionType.Top:
+                        angle = 0f;
+                        break;
+                    case DoorDirectionType.Bottom:
+                        angle = 180f;
+                        break;
+                    default:
+                        Debug.LogWarning($"{room}'s {door} isn't setting {nameof(DoorDirectionType)}.");
+                        break;
+                }
+
+                foreach (var col in Physics2D.OverlapBoxAll(door.transform.position, new Vector2(1f, 8f), angle, 1 << LayerDatas.ROOM_LAYER))
+                {
+                    if (col.transform == door.transform.parent) continue;
+
+                    RoomBase connectedRoom = col.GetComponent<RoomBase>();
+                    if (connectedRoom)
+                    {
+                        door.SetConnectedRoom(connectedRoom);
+                    }
+                    else
+                    {
+                        Debug.LogError($"{col.gameObject.name} don't have RoomBase component.");
+                    }
+                }
+            }
+        }
+    }
+}
