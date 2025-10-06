10132ea810d9f932a0c99ca11a4d5b1df7ce8ed8
OrakGarak2 <888seonghun888@gmail.com>
2025-10-06 11:53:02 +0200

    Add | Stair Scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
---
+++ Assets/Scripts/Stair.cs
@@ -0,0 +1,25 @@
+// System
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+// Unity
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// HLO
+using HLO.Layer;
+
+public class Stair : MonoBehaviour
+{
+    [SerializeField] private SceneAsset nextScene;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerDatas.PLAYER_LAYER)
+        {
+            SceneManager.LoadScene(nextScene.name);
+        }
+    }
+}
